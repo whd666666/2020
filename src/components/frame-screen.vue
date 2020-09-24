af33a5b3a8b4ecbaf2fd32a98cb4d23ce8c6e6aa
王鸿达 <1902933046@qq.com>
2020-09-24 20:31:10 +0800

    初次提交

---
+++ src/components/frame-screen.vue
@@ -0,0 +1,30 @@
+<template>
+  
+  <div class="frame-screen-list"  v-if="type==='list'">
+    <div class="swiper">swiper</div>
+    <div class="item">item</div>
+    <div class="item">item</div>
+    <div class="item">item</div>
+    <div class="item">item</div>
+    <div class="item">item</div>
+    <div class="item">item</div>
+  </div>
+  
+  <div class="frame-screen-detail" v-else>
+
+  </div>
+  
+</template>
+
+<script>
+  export default{
+    props:{
+      type:{
+        type:String,
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
---
+++ src/pages/no-page.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="no-page">
+   <img src="../assets/img/16.jpg" alt="">
+       <router-link to="/home">回到首页</router-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    name:'no-page',
+    props:{},
+    data(){
+      return {
+        
+      }
+    },
+    components:{},
+    mounted(){},
+    updated(){},
+    methods:{}
+  }
+</script>
+
+<style>
+  .no-page{
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+  }
+  img{
+	  width: 100%;
+	  height: 100%;
+  }
+</style>
